9d0c5be39ceaa85a48afd90cbc1367cdc2794d94
ivansch <schmittivan222@gmail.com>
2018-09-10 20:11:59 -0300

    perfil y logos

---
+++ perfil.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Perfil</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,500,700,800" rel="stylesheet">
+		<link rel="stylesheet" href="css/perfil.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <article class="">
+          <p class="usuario">Ivan Schmitt</p>
+          <img src="img/user1.png" class="imguser">
+        </article>
+      </nav>
+    </header>
+    <section>
+      <article class="rectablanco">
+        <div class="rectaazul">
+          <p class="letra">S</p>
+        </div>
+      </article>
+      <h2 class="nombreusu">Ivan Schmitt</h2>
+      <p class="parr">schmittivan222@gmail.com</p>
+      <p class="parr">2243525721</p>
+      <p class="parr">La Plata, Buenos Aires ,Argentina.</p>
+      <article>
+      <button type="button" class="botonazul">
+        <p class="textblanco">Cerrar sesión</p>
+      </button>
+      <button type="button" class="botonblanco">
+        <p class="textazul">Editar perfil</p>
+      </button>
+
+      </article>
+    </section>
+    <footer>
+      <a class="qs" href="#">¿Quienes somos?</a>
+      <a class="pf" href="#">Preguntas frecuentes</a>
+      <img class="logoface" src="img/facelogo.png" alt="">
+      <img class="logoinsta" src="img/instalogo.png" alt="">
+      <div class="contacto">
+        <a href="#">contactanos</a>
+      </div>
+
+    </footer>
+  </body>
+</html>

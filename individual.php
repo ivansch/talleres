62536dc12f2f8a6603122ddabbfe361ff2c59dc8
ivansch <schmittivan222@gmail.com>
2018-10-08 13:40:09 -0300

    vista-individual

---
+++ individual.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php require_once('head.php') ?>
+		<title>Individual</title>
+		<link rel="stylesheet" href="css/talleres.css">
+  </head>
+  <body>
+    <main class="principal">
+      <h2 class="tit-taller">GPS-Taller</h2>
+      <img class="img-taller"src="img/Taller04.jpg" alt="">
+      <p class="des-tall">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+      <p class="califi">4.6</p>
+      <img class="star-vi" src="img/star.png" alt="">
+      <p class="contac">Ponerme e contacto</p>
+      <p class="especialidad">Direccion y especialidad</p>
+      <img class="msnface" src="img/messen.png" alt="">
+      <img class="wsp" src="img/whatsapp-logo.png" alt="">
+      <img class="tools" src="img/tools.png" alt="">
+      <article class="ubicacion">
+        <img class="logomapa"src="img/logomapa.png" alt="">
+        <p class="dire">La Plata, calle 60 nro 876</p>
+        <p class="nomface">Tallergps</p>
+        <p class="numwsp">221565221</p>
+        <p class="espec">Chapa y pintura</p>
+      </article>
+
+
+    </main>
+  </body>
+</html>
